e695f0b9ec6d8b3bb318fe3f4144283671217439
rick-sys <ricksanchezy2x@gmail.com>
2024-03-03 19:24:27 +0100

    fff

--- app/stripe/subscription-webhook/route.ts
+++ app/stripe/subscription-webhook/route.ts
@@ -27,9 +27,9 @@ const fiveCreditsPriceId = process.env.STRIPE_PRICE_ID_FIVE_CREDITS as string;
 const creditsPerPriceId: {
   [key: string]: number;
 } = {
-  [oneCreditPriceId]: 1,
-  [threeCreditsPriceId]: 3,
-  [fiveCreditsPriceId]: 5,
+  [oneCreditPriceId]: 50,
+  [threeCreditsPriceId]: 100,
+  [fiveCreditsPriceId]: 150,
 };
 
 export async function POST(request: Request) {

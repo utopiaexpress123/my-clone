2e781396a7f8277890a302bef01854edb8225678
rick-sys <ricksanchezy2x@gmail.com>
2024-03-29 20:54:44 +0100

    sss

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,12 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server';
-import { Database } from './types/supabase';
+import type { NextRequest } from 'next/server'
+import { Database } from './types/supabase'
 
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
-});
- 
-export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
-};
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
+  await supabase.auth.getSession()
+  return res
+}

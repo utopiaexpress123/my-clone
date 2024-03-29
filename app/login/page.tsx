4f466ab5cf09d85ce72f584d6c8532286115a6f5
rick-sys <ricksanchezy2x@gmail.com>
2024-03-29 20:48:43 +0100

    jjjj

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -39,6 +39,23 @@ export default async function Login() {
             />
             <Button>Continue</Button>
             <Messages />
+
+<div id="g_id_onload"
+     data-client_id="https://faceid.utopia.express/overview"
+     data-context="signin"
+     data-ux_mode="popup"
+     data-callback="handleSignInWithGoogle"
+     data-itp_support="true">
+</div>
+
+<div className="g_id_signin"
+     data-type="standard"
+     data-shape="pill"
+     data-theme="outline"
+     data-text="signin_with"
+     data-size="large"
+     data-logo_alignment="left">
+</div>
           </CardContent>
           <CardFooter>
             <p className="text-sm">
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,22 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
 
-import type { NextRequest } from 'next/server'
-import { Database } from './types/supabase'
+import type { NextRequest } from 'next/server';
+import { Database } from './types/supabase';
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
-  await supabase.auth.getSession()
-  return res
-}
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  // publicRoutes: ['/anyone-can-visit-this-route'],
+});
+ 
+export const config = {
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)"
+  ]
+};
